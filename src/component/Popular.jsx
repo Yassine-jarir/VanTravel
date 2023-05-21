ba569dbaa33515ec67cd4a381d55f052e95aa78e
yassine <mugiwarajr0@gmail.com>
2023-05-21 11:47:20 +0100

    1

--- src/component/Popular.jsx
+++ src/component/Popular.jsx
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
+
 function Popular() {
-  return <div>Popular</div>;
+  // async : rander data befor render eny things else
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apikey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    console.log(data);
+  };
+
+  return <div>ss</div>;
 }
 
 export default Popular;
