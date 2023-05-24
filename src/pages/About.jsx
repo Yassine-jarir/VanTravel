dc823adf59a3525ac491e0d98c50947538ca7b8c
yassine <mugiwarajr0@gmail.com>
2023-05-24 16:36:46 +0100

    test

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-
+import aboutimg from "../assets/about-hero.png";
 export default function About() {
   return (
     <div className="about-page-container">
-      <img src="../assets/about-hero-png" className="about-hero-image" />
+      <img src={aboutimg} className="about-hero-image" />
       <div className="about-page-content">
         <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
         <p>
