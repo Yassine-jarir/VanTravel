b469bf267ba6e6b2ba67cff71eb10dd194180f00
yassine <mugiwarajr0@gmail.com>
2023-05-26 19:00:43 +0100

    netfily

--- src/pages/Host/vanshoast/Photos.jsx
+++ src/pages/Host/vanshoast/Photos.jsx
@@ -1,5 +1,8 @@
+import { useOutletContext } from "react-router-dom";
+
 function Photos() {
-  return <div>Photos</div>;
+  const { currentVan } = useOutletContext();
+  return <img src={currentVan.imageUrl} className="host-van-detail-image" />;
 }
 
 export default Photos;
